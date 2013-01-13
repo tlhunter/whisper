0b15f5fa58809cf16cbce17a627d74a060a29a9d
Thomas Hunter <tlhunter@gmail.com>
2013-01-13 14:46:43 -0500

    basic messaging works

--- public/main.js
+++ public/main.js
@@ -7,25 +7,71 @@ $(function() {
     var $enableGeo = $('#enable-geo');
     var $data = $('#data');
 
+    // My last known location
+    var coords = {};
+
+    // Display a message in the DOM
+    var displayMessage = function(data) {
+        // This probably only works in Chrome...
+        var date = new Date(data.time).toLocaleTimeString();
+        $messages.prepend('<div class="message size-' + data.size + '"><time>' + date + '</time>: ' + data.body + '</div>');
+    };
+
+    // I received a message from the server
+    socket.on('message-to-client', function (data) {
+        console.log(data);
+        displayMessage(data);
+    });
+
+    // One of the size radio buttons were cliced
     $('#size').click(function() {
         var size = $('#size input:checked').val();
         $messageInput.removeClass().addClass('size-'+size);
     });
 
-    $compose.submit(function(event) {
-        event.preventDefault();
-        var $message = $messageInput.val() || '';
-        var $size = $('#size input:checked').val() || null;
-    });
+    var setNewCoordinates = function(c) {
+        $data.html("Latitude: " + c.latitude + "<br />Longitude: " + c.longitude + "<br />Accuracy: " + c.accuracy);
+        coords = c;
+    };
 
     var handleGeoLocation = function(pos) {
-        $data.html("Latitude: " + pos.coords.latitude + "<br />Longitude: " + pos.coords.longitude + "<br />Accuracy: " + pos.coords.accuracy);
-        console.log(pos);
+        setNewCoordinates(pos.coords);
     };
 
     var initiateGeoLocation = function() {
         navigator.geolocation.getCurrentPosition(handleGeoLocation);
     };
 
-    $enableGeo.click(initiateGeoLocation);
+    $compose.submit(function(event) {
+        event.preventDefault();
+        initiateGeoLocation();
+        var message = $messageInput.val() || '';
+        $messageInput.val('');
+        var size = $('#size input:checked').val() || null;
+        if (!message || !size) {
+            displayMessage({
+                time: new Date(),
+                size: 0,
+                body: "You must provide a message and select a size."
+            });
+            return;
+        }
+
+        if (!coords.latitude || !coords.longitude) {
+            displayMessage({
+                time: new Date(),
+                size: 0,
+                body: "I am unsure of your position."
+            });
+            return;
+        }
+
+        socket.emit('message-to-server', {
+            size: parseInt(size, 10),
+            body: message,
+            coords: coords
+        });
+    });
+
+    initiateGeoLocation();
 });
--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var web = express();
 var server = require('http').createServer(web);
-var socket = require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+var geohash = require('ngeohash');
+
+//var redis = require('redis');
+//var redisClient = redis.createClient();
+
+var messages = [];
 
 web.use('/', express.static(__dirname + '/public'));
 
 server.listen(80);
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('message-to-client', {
+        time: new Date(),
+        size: 0,
+        body: "Socket Connection Established",
+    });
+
+    socket.on('message-to-server', function(data) {
+        var size = parseInt(data.size, 10);
+        var time = new Date();
+        var body = data.body.substring(0, 255);
+        var coords = {
+            lat: data.coords.latitude,
+            lon: data.coords.longitude
+        };
+
+        var hash = geohash.encode(coords.lat, coords.lon);
+
+        // Transmit to a few different geogash room names, instead of everyone
+        console.log(size, time, body, coords, hash);
+        io.sockets.emit('message-to-client', {
+            time: time,
+            size: size,
+            body: body
+        });
+    });
+});
+
